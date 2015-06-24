0c5879ab0fae019f68670b4d9982ebf18a4d98df
Marcus Olsson <ninjasinventedphysics@gmail.com>
2015-06-24 21:28:48 +0200

    Add Scanner for tokenizing

---
+++ scan.go
@@ -0,0 +1,178 @@
+package gotro
+
+import (
+	"bufio"
+	"bytes"
+	"io"
+	"unicode"
+)
+
+// Token represents a lexical token.
+type Token int
+
+// Tokens
+const (
+	ILLEGAL Token = iota
+	WS
+	EOF
+
+	IDENT
+	INTEGER
+
+	LEFTPAREN
+	RIGHTPAREN
+)
+
+func (t Token) String() string {
+	switch t {
+	case ILLEGAL:
+		return "ILLEGAL"
+	case WS:
+		return "WS"
+	case EOF:
+		return "EOF"
+	case IDENT:
+		return "IDENT"
+	case INTEGER:
+		return "INTEGER"
+	case LEFTPAREN:
+		return "LEFTPAREN"
+	case RIGHTPAREN:
+		return "RIGHTPAREN"
+	default:
+		return ""
+	}
+}
+
+var eof = rune(0)
+
+// Scanner represents a lexical scanner.
+type Scanner struct {
+	r *bufio.Reader
+}
+
+// NewScanner returns a new instance of Scanner.
+func NewScanner(r io.Reader) *Scanner {
+	return &Scanner{
+		r: bufio.NewReader(r),
+	}
+}
+
+func (s *Scanner) read() rune {
+	ch, _, err := s.r.ReadRune()
+	if err != nil {
+		return eof
+	}
+	return ch
+}
+
+func (s *Scanner) unread() {
+	_ = s.r.UnreadRune()
+}
+
+// Scan returns the next token and its value.
+func (s *Scanner) Scan() (Token, string) {
+	ch := s.read()
+
+	if unicode.IsSpace(ch) {
+		s.unread()
+		return s.scanSpace()
+	} else if unicode.IsLetter(ch) {
+		s.unread()
+		return s.scanIdent()
+	} else if unicode.IsDigit(ch) {
+		s.unread()
+		return s.scanInt()
+	} else if isQuote(ch) {
+		s.unread()
+		return s.scanQuote()
+	} else if isParen(ch) {
+		if ch == '(' {
+			return LEFTPAREN, "("
+		} else if ch == ')' {
+			return RIGHTPAREN, ")"
+		}
+	}
+
+	return ILLEGAL, ""
+}
+
+func isQuote(ch rune) bool {
+	return ch == '"'
+}
+
+func isParen(ch rune) bool {
+	return ch == '(' || ch == ')'
+}
+
+func (s *Scanner) scanSpace() (Token, string) {
+	var buf bytes.Buffer
+	buf.WriteRune(s.read())
+
+	for {
+		if ch := s.read(); ch == eof {
+			break
+		} else if !unicode.IsSpace(ch) {
+			s.unread()
+			break
+		} else {
+			buf.WriteRune(ch)
+		}
+	}
+
+	return WS, " "
+}
+
+func (s *Scanner) scanIdent() (Token, string) {
+	var buf bytes.Buffer
+	buf.WriteRune(s.read())
+
+	for {
+		if ch := s.read(); ch == eof {
+			break
+		} else if !unicode.IsLetter(ch) {
+			s.unread()
+			break
+		} else {
+			_, _ = buf.WriteRune(ch)
+		}
+	}
+
+	return IDENT, buf.String()
+}
+
+func (s *Scanner) scanInt() (Token, string) {
+	var buf bytes.Buffer
+	buf.WriteRune(s.read())
+
+	for {
+		if ch := s.read(); ch == eof {
+			break
+		} else if !unicode.IsDigit(ch) {
+			s.unread()
+			break
+		} else {
+			_, _ = buf.WriteRune(ch)
+		}
+	}
+
+	return INTEGER, buf.String()
+}
+
+func (s *Scanner) scanQuote() (Token, string) {
+	var buf bytes.Buffer
+	buf.WriteRune(s.read())
+
+	for {
+		if ch := s.read(); ch == eof {
+			break
+		} else if isQuote(ch) {
+			_, _ = buf.WriteRune(ch)
+			break
+		} else {
+			_, _ = buf.WriteRune(ch)
+		}
+	}
+
+	return IDENT, buf.String()
+}
---
+++ scan_test.go
@@ -0,0 +1,120 @@
+package gotro
+
+import (
+	"log"
+	"strings"
+	"testing"
+)
+
+type tokenValuePair struct {
+	Token Token
+	Value string
+}
+
+var tests = []struct {
+	In  string
+	Out []tokenValuePair
+}{
+	{
+		In: "test",
+		Out: []tokenValuePair{
+			{Token: IDENT, Value: "test"},
+		},
+	},
+	{
+		In: `key "value"`,
+		Out: []tokenValuePair{
+			{Token: IDENT, Value: "key"},
+			{Token: WS, Value: " "},
+			{Token: IDENT, Value: `"value"`},
+		},
+	},
+	{
+		In: `game (
+			name "Test Name"
+		)`,
+		Out: []tokenValuePair{
+			{Token: IDENT, Value: "game"},
+			{Token: WS, Value: " "},
+			{Token: LEFTPAREN, Value: `(`},
+			{Token: WS, Value: " "},
+			{Token: IDENT, Value: "name"},
+			{Token: WS, Value: " "},
+			{Token: IDENT, Value: `"Test Name"`},
+			{Token: WS, Value: " "},
+			{Token: RIGHTPAREN, Value: `)`},
+		},
+	},
+	{
+		In: `rom ( name "Test Name" size 2621440 )`,
+		Out: []tokenValuePair{
+			{Token: IDENT, Value: "rom"},
+			{Token: WS, Value: " "},
+			{Token: LEFTPAREN, Value: `(`},
+			{Token: WS, Value: " "},
+			{Token: IDENT, Value: "name"},
+			{Token: WS, Value: " "},
+			{Token: IDENT, Value: `"Test Name"`},
+			{Token: WS, Value: " "},
+			{Token: IDENT, Value: "size"},
+			{Token: WS, Value: " "},
+			{Token: INTEGER, Value: "2621440"},
+			{Token: WS, Value: " "},
+			{Token: RIGHTPAREN, Value: `)`},
+		},
+	},
+	{
+		In: `game (
+			name "Test Name"
+			description "Test Description"
+			rom ( name "Test Name" size 2621440 )
+		)`,
+		Out: []tokenValuePair{
+			{Token: IDENT, Value: "game"},
+			{Token: WS, Value: " "},
+			{Token: LEFTPAREN, Value: `(`},
+			{Token: WS, Value: " "},
+			{Token: IDENT, Value: "name"},
+			{Token: WS, Value: " "},
+			{Token: IDENT, Value: `"Test Name"`},
+			{Token: WS, Value: " "},
+			{Token: IDENT, Value: "description"},
+			{Token: WS, Value: " "},
+			{Token: IDENT, Value: `"Test Description"`},
+			{Token: WS, Value: " "},
+			{Token: IDENT, Value: "rom"},
+			{Token: WS, Value: " "},
+			{Token: LEFTPAREN, Value: `(`},
+			{Token: WS, Value: " "},
+			{Token: IDENT, Value: "name"},
+			{Token: WS, Value: " "},
+			{Token: IDENT, Value: `"Test Name"`},
+			{Token: WS, Value: " "},
+			{Token: IDENT, Value: "size"},
+			{Token: WS, Value: " "},
+			{Token: INTEGER, Value: "2621440"},
+			{Token: WS, Value: " "},
+			{Token: RIGHTPAREN, Value: `)`},
+		},
+	},
+}
+
+func TestScanner(t *testing.T) {
+	for _, tt := range tests {
+		r := strings.NewReader(tt.In)
+		s := NewScanner(r)
+
+		for _, p := range tt.Out {
+
+			tok, str := s.Scan()
+			log.Println(tok, str)
+			if tok != p.Token {
+				t.Fatalf("unexpected token: %s, expected %s", tok, p.Token)
+			}
+
+			if str != p.Value {
+				t.Fatalf("unexpected value: %s, expected %s", str, p.Value)
+			}
+		}
+	}
+}
